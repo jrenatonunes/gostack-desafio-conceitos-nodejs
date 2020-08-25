eb3098868ed1a306a531c2b2fb208826b56c459d
Jose Renato Nunes <jrenato.nunes@gmail.com>
2020-08-25 19:52:03 -0300

    Route that allow to update a repositoty

--- src/app.js
+++ src/app.js
@@ -35,8 +35,27 @@ app.post("/repositories", (request, response) => {
   return response.status(201).json(repository);
 });
 
+
+
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id } = request.params;
+  const {title, url, techs} = request.body;
+
+  if ( !title && !url && !techs) {
+    return response.status(400).json({error: 'Bad request: title, url and tecks not povide'});
+  }
+
+  const indexRepository = repositories.findIndex(repository => repository.id === id);
+  if ( indexRepository < 0 ) {
+    return response.status(400).json({error: 'Repository not exists'});
+  }
+
+  // Atualiza o titulo, url ou techs caso os mesmos sejam fornecidos
+  title && (repositories[indexRepository].title = title);
+  url && (repositories[indexRepository].url = url);
+  techs && (repositories[indexRepository].techs = techs);
+
+  return response.status(200).json(repositories[indexRepository]);
 });
 
 
